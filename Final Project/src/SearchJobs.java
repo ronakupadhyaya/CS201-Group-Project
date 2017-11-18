c66a4aa6d10009f7c8a611f01aea5fda1364c4a5
Mai-Nhien <mainhied@usc.edu>
2017-11-18 14:13:32 -0800

    search jobs with json

--- Final Project/src/SearchJobs.java
+++ Final Project/src/SearchJobs.java
@@ -15,7 +15,7 @@ public class SearchJobs extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String keyword = request.getParameter("keyword");
+		
 	}
 }
